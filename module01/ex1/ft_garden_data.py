c43602b4758dceb4d23580d0e4acbfeb8db5ff0b
Yiannis Dimitrakopoulos <ydimitra@f0r2s16.codam.nl>
2025-12-10 18:08:06 +0100

    first ex

---
+++ module01/ex1/ft_garden_data.py
@@ -0,0 +1,16 @@
+class Plant:
+    def __init__(plant, name, height, age):
+        plant.name = name
+        plant.height = height
+        plant.age = age
+
+if __name__ == "__main__":
+    p1 = Plant("Rose", "25cm", "30 days old")
+    p2 = Plant("Sunflower", "80cm", "45 days old")
+    p3 = Plant("Cactus", "15cm", "120 days old")
+    print("=== Garden Plant Registry ===")
+    print(f"{p1.name}: {p1.height}, {p1.age}")
+    print(f"{p2.name}: {p2.height}, {p2.age}")
+    print(f"{p3.name}: {p3.height}, {p3.age}")
+
+
---
+++ module01/ex2/ft_plant_growth.py
@@ -0,0 +1,8 @@
+class Plant:
+    def __init__(plant, name, height, age):
+        plant.name = name
+        plant.height = height
+        plant.age = age
+
+if __name__ == "__main__":
+
