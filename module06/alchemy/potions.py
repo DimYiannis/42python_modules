b6de0f1b5e356ca196b5a8d73eb8dc25a5142ea3
DimYiannis <johndim99@gmail.com>
2026-01-08 11:26:21 +0200

    part 2

--- module06/alchemy/potions.py
+++ module06/alchemy/potions.py
@@ -11,3 +11,15 @@ def invisibility_potion():
 
 def wisdom_potion():
     return "Wisdom potion brewed with all elements: [all_four_results]"
+
+def healing_potion(): 
+    return "Healing potion brewed with [fire_result] and [water_result]"
+
+def strength_potion(): 
+    return "Strength potion brewed with [earth_result] and [fire_result]"
+
+def invisibility_potion():
+    return "Invisibility potion brewed with [air_result] and [water_result]"
+
+def wisdom_potion(): 
+    return "Wisdom potion brewed with all elements: [all_four_results]"
