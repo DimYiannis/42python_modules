d7dc71bc3c80f7c79598612880f79c37dc4d8f7b
DimYiannis <johndim99@gmail.com>
2026-01-09 23:42:57 +0200

    display in main.py

---
+++ module07/ex2/main.py
@@ -0,0 +1,23 @@
+
+from .EliteCard import EliteCard
+
+if __name__ = "__main__"
+    print("\n=== DataDeck Ability System ===")
+
+    elite = EliteCard("Arcane Warrior", 6, "Legendary", attack_power = 5)
+
+    print("\nEliteCard capabilities:")
+    print("- Card:", [func for func in dir(EliteCard) if func in ["attack", "defend", "get_combat_stats"]])
+    print("- Compatable:", [func for func in dir(EliteCard) if func in ["attack", "defend", "get_combat_stats"]])
+    print("- Magical:", [func for func in dir(EliteCard) if func in ["cast_spell", "channel_mana", "get_magic_stats"]])
+
+    print("\nPlaying Arcane Warrior (Elite Card):")
+    print("Combat phase:")
+    print("Attack result:", elite.attack("Enemy"))
+    print("Defence result:", elite.defend(5))
+
+    print("Magic phase:")
+    print("Spell cast:", elite.cast_spell("Fireball", ["Enemy1", "Enemy2"]))
+    print("Mana channel", elite.channel_mana(3))
+
+    print("\nMultiple interface implementation successful!!")
