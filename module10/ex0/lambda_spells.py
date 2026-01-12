686cd1d360696d08ebc6e67d85ee86bf5c36e23a
Yiannis Dimitrakopoulos <ydimitra@f0r3s11.codam.nl>
2026-01-12 17:04:28 +0100

    completed the making of the functions need to check for flake8 too

--- module10/ex0/lambda_spells.py
+++ module10/ex0/lambda_spells.py
@@ -22,11 +22,23 @@ def power_filter(mages: list[dict], min_power: int) -> list[dict]:
 
 
 def spell_transformer(spells: list[str]) -> list[str]:
-    pass
+    """
+    adding * as prefix and sufix
+    """
+    result = list(map(lambda x: '*' + x + '*', spells))
+    return result
 
 
 def mage_stats(mages: list[dict]) -> dict:
-    pass
+    """
+        cant use lambda in sum 
+        used round func to determine the number of decimals
+    """
+    strongest = max(mages, key=lambda x: x['power'])
+    weakest = min(mages, key=lambda x: x['power'])
+    avg_power = round(sum(x['power'] for x in mages) / len(mages), 2)
+    result = {'max_power': strongest, 'min_power': weakest, 'avg_power': avg_power}
+    return result
 
 
 if __name__ == "__main__":
@@ -36,6 +48,19 @@ if __name__ == "__main__":
         {"name": "Shadow Cloak", "power": 85, "type": "dark"},
     ]
 
+    names = [
+        "Ariana", "Kai", "Luna", "Ezra", "Maya",
+        "Finn", "Nova", "Leo", "Isla", "Jasper",
+        "Elara", "Ronan", "Soren", "Freya", "Atlas",
+        "Clara", "Orion", "Ivy", "Dashiell", "Zara"
+    ]
     print("\nTesting artifact sorter...")
     print("\nartifact sorter:", artifact_sorter(artifacts))
     print("\npower filter:", power_filter(artifacts, 85))
+    print("\nspell transformer:", spell_transformer(names))
+
+    print("\nmage stats:")
+    stats = mage_stats(artifacts)
+    print(f"max power:{stats['max_power']}")
+    print(f"min power:{stats['min_power']}")
+    print(f"avg power:{stats['avg_power']}")
