383d9fede6696ff19ffe54183eb0f7968554caca
Yiannis Dimitrakopoulos <ydimitra@f0r3s11.codam.nl>
2026-01-13 11:46:25 +0100

    managed to understand the distinction between passing a func and calling it, importand to be able to use funcs in a lambda function

--- module10/ex1/higher_magic.py
+++ module10/ex1/higher_magic.py
@@ -2,11 +2,11 @@
 
 
 def spell_combiner(spell1: callable, spell2: callable) -> callable:
-    lambda x: (spell1(x), spell2(x))
+    return lambda x: (spell1(x), spell2(x))
 
 
 def power_amplifier(base_spell: callable, multiplier: int) -> callable:
-    pass
+    return lambda x: base_spell(x) * multiplier
 
 
 def conditional_caster(condition: callable, spell: callable) -> callable:
@@ -17,7 +17,22 @@ def spell_sequence(spells: list[callable]) -> callable:
     pass
 
 
+def add_two(x: int) -> int:
+    result = x + 2
+    return result
+
+def add_3(x: int) -> int:
+    result = x + 3
+    return result
 
 
 if __name__ == "__main__":
+ 
+    result1 = spell_combiner(add_two, add_3)
+    print(f"spell combiner:", result1(0))
+
+    result2 = power_amplifier(add_3, 3)
+    print("power_amplifier:", result2(0))
+
+
 
