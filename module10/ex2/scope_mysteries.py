d019e593be8154f0427268621303d4f1d0cb56a4
Yiannis Dimitrakopoulos <ydimitra@f0r3s11.codam.nl>
2026-01-13 18:10:25 +0100

    last push at codam for today, need to fix last func in ex2

--- module10/ex2/scope_mysteries.py
+++ module10/ex2/scope_mysteries.py
@@ -10,25 +10,66 @@ def mage_counter() -> callable:
         without global var
     """
     def counter():
-        nonlocal count += 1
+        """
+            used nonlocal for enclosing Scope 
+            Variables in an enclosing function but not global
+        """
+        nonlocal count 
+        count += 1
         return count
-    return counter 
+    return counter
 
 
 def spell_accumulator(initial_power: int) -> callable:
-    pass
+    
+    base_power = initial_power
+    
+    def increase():
+        nonlocal base_power
+        base_power += 150
+        return base_power
+    return increase
 
 
 def enchantment_factory(enchantment_type: str) -> callable:
-    pass
+    def inner(item: str):
+        print(f"{enchantment_type} {item}")
+    return inner
 
 
 def memory_vault() -> dict[str, callable]:
-    pass
 
+    data = {}
+
+    def store(key: str, value):
+        nonlocal data
+        data["key"] = value
+
+    def recall(key: str):
+        nonlocal data
+        for item in data:
+            if data.get('key') == key
 
 
 if __name__ == "__main__":
 
-    for num in range(0,5):
-        print(mage_counter)
+    print("\nTesting mage counter:")
+    c = mage_counter()
+    for num in range(1,4):
+        print(f"Call {num}:", c())
+
+    print("\nTetsing spell_accumulator:")
+    inner = spell_accumulator(0)
+    for i in range(1,4):
+        print(f"Call {i}:", inner())
+
+    print("\nTesting enchantment_factory:")
+    Flaming = enchantment_factory("Flaming")
+    Frozen = enchantment_factory("Frozen")
+    Flaming("sword")
+    Frozen('shield')
+    Flaming('potion')
+
+    print("\nTesting memory_vault")
+
+
