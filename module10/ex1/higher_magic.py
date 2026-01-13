38369188c92602d4268d9916ac73b7066fd6f946
Yiannis Dimitrakopoulos <ydimitra@f0r3s11.codam.nl>
2026-01-13 12:59:40 +0100

    finished, fixed the display format

--- module10/ex1/higher_magic.py
+++ module10/ex1/higher_magic.py
@@ -10,11 +10,12 @@ def power_amplifier(base_spell: callable, multiplier: int) -> callable:
 
 
 def conditional_caster(condition: callable, spell: callable) -> callable:
-    pass
+    return lambda x: spell(x) if condition(x) == True else "Spell fizzled"
 
 
 def spell_sequence(spells: list[callable]) -> callable:
-    pass
+    return lambda x: [f(x) for f in spells]
+# could also do list(map(lambda f: f(x), spells ))
 
 
 def add_two(x: int) -> int:
@@ -25,14 +26,37 @@ def add_3(x: int) -> int:
     result = x + 3
     return result
 
+def maybe(num): 
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
 
 if __name__ == "__main__":
  
     result1 = spell_combiner(add_two, add_3)
-    print(f"spell combiner:", result1(0))
+    print("\nTesting spell combiner:")
+    print("spell combiner result:", result1(0))
 
     result2 = power_amplifier(add_3, 3)
-    print("power_amplifier:", result2(0))
+    print("\nTetsing power amplifier:")
+    print("power_amplifier result:", result2(0))
+
+    #checking if maybe works correctly and then proceed with the function
+    print(maybe(3))
+    result3 = conditional_caster(maybe, add_two)
+    print("\nTesting conditional_caster:")
+    print("conditional_caster result", result3(3))
+
+    listt = [add_two, add_3, maybe]
+    result4 = spell_sequence(listt)
+    print("\nTesting spell_sequence")
+    print("spell sequence result:",result4(2))
+
+
+
+
 
 
 
